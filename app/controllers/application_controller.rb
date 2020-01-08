926d829546508ed5765c134a405fafce7728a5f6
Nitin N <ntntvtgit@gmail.com>
2020-01-08 20:14:30 +0530

    completed adding user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:account_update) { |u|
+  	  u.permit(:avatar, :email, :password, :password_confirmation, :current_password)
+  	}
+  end
 end
